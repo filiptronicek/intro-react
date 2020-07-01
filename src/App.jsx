e37a6fe5186a69d09ad7d11c0ff329c95cdfda99
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-01 17:58:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -75,9 +75,7 @@ class App extends React.Component {
         />
       );
     }
-
-    /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -86,7 +84,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
